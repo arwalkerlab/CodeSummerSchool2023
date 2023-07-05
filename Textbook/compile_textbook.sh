deb9f6b61d5f2db783f57e2a20a65b8891476655
Mark A. Hix <markahix@gmail.com>
2023-07-05 12:31:46 -0400

    updates

---
+++ Textbook/compile_textbook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pdflatex --interaction=nonstopmode main.tex
+pdflatex --interaction=nonstopmode main.tex
+bibtex main.aux
+pdflatex --interaction=nonstopmode main.tex
+
+mv main.pdf CodeSummerSchool2023.pdf
+rm main.aux
+rm main.bbl
+rm main.blg
+rm main.log
+rm main.out
+rm main.pyg
+rm main.toc
